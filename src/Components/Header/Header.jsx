c79ff169fed6958066dff1bbdffe4670afea3563
shafiul-osd <mdshafiulislamsafi2506@gmail.com>
2024-03-17 00:21:25 +0600

    nav menu updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,25 +12,20 @@ function Header() {
               </svg>
             </div>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li><a>Item 3</a></li>
+              <li><a>Home</a></li>
+              <li><a>Recipes</a></li>
+              <li><a>About</a></li>
+              <li><a>Search</a></li>
             </ul>
           </div>
           <a className="btn btn-ghost text-xl">Recipe Calories</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-            <li><a>Item 3</a></li>
+              <li><a>Home</a></li>
+              <li><a>Recipes</a></li>
+              <li><a>About</a></li>
+              <li><a>Search</a></li>
           </ul>
         </div>
         <div className="navbar-end">
